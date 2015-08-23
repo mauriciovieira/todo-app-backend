34282ee6e201d7bda3111ad6674a9e76c5877288
Mauricio Vieira <mauricio@mauriciovieira.net>
2015-08-23 18:41:51 -0300

    Use todo-mvc.mauriciovieira.net as frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
   allow do
-    origins 'localhost:9000'
+    origins 'todo-mvc.mauriciovieira.net'
 
     resource '*',
       headers: :any,
